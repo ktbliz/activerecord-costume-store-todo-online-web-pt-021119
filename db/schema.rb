ae95eedd999ffe54d2981be02a8005340bc47600
Katie Blizzard <kblizzard11@gmail.com>
2019-05-11 13:33:38 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
+    t.string "location"
     t.string "theme"
     t.integer "price"
     t.boolean "family_friendly"
